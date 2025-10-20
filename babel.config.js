fa1eb94e896219f049f03d530cd8c46dc5a7e19e
sudhanya00 <33546511+sudhanya00@users.noreply.github.com>
2025-10-21 03:49:00 +0530

    Add Firebase config files to repo for EAS Build and fix NativeWind babel config

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel"
+    ],
     plugins: [
-      "nativewind/babel",
       "react-native-reanimated/plugin"
     ]
   };
