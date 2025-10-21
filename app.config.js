49c6dc52a85273ea7cd75e9bfb8f362e3fc3d146
sudhanya00 <33546511+sudhanya00@users.noreply.github.com>
2025-10-21 21:08:50 +0530

    Fix AdMob App IDs - use hardcoded values instead of env vars

--- app.config.js
+++ app.config.js
@@ -77,8 +77,8 @@ export default () => ({
       [
         "react-native-google-mobile-ads",
         {
-          "android_app_id": process.env.ADMOB_APP_ID_ANDROID || "ca-app-pub-3940256099942544~3347511713",
-          "ios_app_id": process.env.ADMOB_APP_ID_IOS || "ca-app-pub-3940256099942544~1458002511"
+          "android_app_id": "ca-app-pub-2534446887487052~2032082918",
+          "ios_app_id": "ca-app-pub-2534446887487052~2554827090"
         }
       ]
     ],
