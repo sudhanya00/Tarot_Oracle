742a965635b958743a34dc530fd937dc6abf9247
itsjkjass <jashanarora1@gmail.com>
2026-04-29 09:05:20 -0400

    Delete eas-hooks directory

--- eas-hooks/eas-build-pre-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Pre-install script for EAS Build
-set -e
-
-echo "Running pre-install hook..."
-
-# Ensure we have the latest dependencies
-echo "EAS Build pre-install complete"
